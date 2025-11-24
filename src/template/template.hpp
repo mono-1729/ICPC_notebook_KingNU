b78203ad97b9d827faa8039960cd705dcb1bced2
hirakuuuu <m.hiraku1129@gmail.com>
2025-11-24 23:20:33 +0900

    fix template

--- src/template/template.hpp
+++ src/template/template.hpp
@@ -2,10 +2,9 @@
 using namespace std;
 using ll = long long;
 const ll INF = LLONG_MAX / 4;
-#define rep(i, a, b) for(ll i = a; i < (b); i++)
+#define rep(i, a, n) for(ll i = a; i < n; i++)
 #define rrep(i, a, n) for(ll i = a; i >= n; i--)
-#define all(a) begin(a), end(a)
-#define sz(a) ssize(a)
+#define inr(l, x, r) (l <= x && x < r)
 bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
 bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
 
