e207b28de2383fbf43f8af66d9793e95fa641b83
hirakuuuu <m.hiraku1129@gmail.com>
2025-11-24 23:29:38 +0900

    long long -> ll

--- src/math/ChineseRem.hpp
+++ src/math/ChineseRem.hpp
@@ -1,31 +1,28 @@
 inline ll mod(ll a, ll m) { return (a % m + m) % m; }
 
-inline long long mul(long long a, long long b, long long m) {
+inline ll mul(ll a, ll b, ll m) {
    a = mod(a, m);
    b = mod(b, m);
    if(b == 0) return 0;
-   long long res = mul(mod(a + a, m), b >> 1, m);
+   ll res = mul(mod(a + a, m), b >> 1, m);
    if(b & 1) res = mod(res + a, m);
    return res;
 }
 
-// 拡張ユークリッドの互除法
-// ap+bq=gcd(a, b) となる (p, q) を求め、d = gcd(a, b) をリターンします。
-ll extGCD(ll a, ll b, ll& p, ll& q) {
-   if(b == 0) {
-      p = 1;
-      q = 0;
-      return a;
-   }
-   ll d = extGCD(b, a % b, q, p);
-   q -= a / b * p;
+// returns gcd(a, b) and assign x, y to integers
+// s.t. ax + by = gcd(a, b) and |x| + |y| is minimized
+ll extgcd(ll a, ll b, ll& x, ll& y) {
+   // assert(a >= 0 && b >= 0);
+   if(!b) return x = 1, y = 0, a;
+   ll d = extgcd(b, a % b, y, x);
+   y -= a / b * x;
    return d;
 }
 
 // 中国剰余定理
 // リターン値を (r, m) とすると解は x = r (mod. m)
 // 解なしの場合は (0, -1) をリターン
-pll chineseRem(const vector<ll>& b, const vector<ll>& m) {
+pair<ll, ll> chineseRem(const vector<ll>& b, const vector<ll>& m) {
    ll r = 0, M = 1;
    rep(i, 0, (int)b.size()) {
       ll p, q;
--- src/math/Eratosthenes.hpp
+++ src/math/Eratosthenes.hpp
@@ -1,9 +1,9 @@
 int max_num = 1000005;
 vector<int> erat(max_num);
 void init_e() {
-   rep(i, 2, sqrt(max_num - 1)) {
+   for(ll i = 2; i*i <= max_num; i++) {
       if(erat[i] == 0) {
-         for(int j = i * i; j <= max_num - 1; j += i) {
+         for(ll j = i * i; j <= max_num - 1; j += i) {
             if(erat[j] == 0) erat[j] = i;
          }
       }
