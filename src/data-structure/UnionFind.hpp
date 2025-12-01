19835d545d2f637e62a073c3a612b191c3bfcc0e
hirakuuuu <m.hiraku1129@gmail.com>
2025-12-01 14:30:37 +0900

    UnionFind

--- src/data-structure/UnionFind.hpp
+++ src/data-structure/UnionFind.hpp
@@ -1,7 +1,7 @@
-struct UF {
+struct UnionFind {
    vector<int> e;
-   UF(int n) : e(n, -1) {}
-   bool sameSet(int a, int b) { return find(a) == find(b); }
+   UnionFind(int n) : e(n, -1) {}
+   bool same(int a, int b) { return find(a) == find(b); }
    int size(int x) { return -e[find(x)]; }
    int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
    bool join(int a, int b) {
