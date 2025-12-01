c235a61a1dd0f27cd4ae1b953bad0b37b29f4b4d
hirakuuuu <m.hiraku1129@gmail.com>
2025-12-01 16:27:38 +0900

    modint

--- src/modint/modint.hpp
+++ src/modint/modint.hpp
@@ -1,26 +1,34 @@
-const ll mod = 998244353;
+const uint32_t mod = 1000000007;
 struct mm {
-   ll x;
-   mm(ll x_ = 0) : x(x_ % mod) {
-      if(x < 0) x += mod;
-   }
-   friend mm operator+(mm a, mm b) { return a.x + b.x; }
-   friend mm operator-(mm a, mm b) { return a.x - b.x; }
-   friend mm operator*(mm a, mm b) { return a.x * b.x; }
-   friend mm operator/(mm a, mm b) { return a * b.inv(); }
-   // 4 行コピペ  Alt + Shift + クリックで複数カーソル
-   friend mm& operator+=(mm& a, mm b) { return a = a.x + b.x; }
-   friend mm& operator-=(mm& a, mm b) { return a = a.x - b.x; }
-   friend mm& operator*=(mm& a, mm b) { return a = a.x * b.x; }
-   friend mm& operator/=(mm& a, mm b) { return a = a * b.inv(); }
-   mm inv() const { return pow(mod - 2); }
-   mm pow(ll b) const {
-      mm a = *this, c = 1;
-      while(b) {
-         if(b & 1) c *= a;
-         a *= a;
-         b >>= 1;
-      }
-      return c;
-   }
+    uint32_t x;
+    mm() : x(0) {}
+    template<class T> mm(T x_) : x(x_ % mod) {
+        if(x >= mod) x += mod;
+    }
+    friend mm operator+(mm a, mm b) {
+        a.x += b.x;
+        if(a.x >= mod) a.x -= mod;
+        return a;
+    }
+    friend mm operator-(mm a, mm b) {
+        a.x -= b.x;
+        if(a.x >= mod) a.x += mod;
+        return a;
+    }
+    friend mm operator*(mm a, mm b) { return (uint64_t)a.x * b.x; }
+    friend mm operator/(mm a, mm b) { return a * b.inv(); }
+    friend mm& operator+=(mm& a, mm b) { return a = a + b; }
+    friend mm& operator-=(mm& a, mm b) { return a = a - b; }
+    friend mm& operator*=(mm& a, mm b) { return a = a * b; }
+    friend mm& operator/=(mm& a, mm b) { return a = a * b.inv(); }
+    mm inv() const { return pow(mod - 2); }
+    mm pow(ll b) const {
+        mm a = *this, c = 1;
+        while(b) {
+            if(b & 1) c *= a;
+            a *= a;
+            b >>= 1;
+        }
+        return c;
+    }
 };
