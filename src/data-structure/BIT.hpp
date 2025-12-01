26ffd87980ec49a9800e855ddc5292d37bf7d308
hirakuuuu <m.hiraku1129@gmail.com>
2025-12-01 14:20:15 +0900

    BIT と SegmentTree を修正

--- src/data-structure/BIT.hpp
+++ src/data-structure/BIT.hpp
@@ -1,22 +1,25 @@
 struct BIT {
-   vector<ll> a;
-   BIT(ll n) : a(n + 1) {}
-   void add(ll i, ll x) {  // A[i] += x
-      i++;
-      while(i < sz(a)) {
-         a[i] += x;
-         i += i & -i;
-      }
-   }
-   ll sum(ll r) {
-      ll s = 0;
-      while(r) {
-         s += a[r];
-         r -= r & -r;
-      }
-      return s;
-   }
-   ll sum(ll l, ll r) {  // sum of A[l, r)
-      return sum(r) - sum(l);
-   }
+    vector<ll> a;
+    BIT(ll n) : a(n + 1) {}
+    // A[i] += x
+    void add(ll i, ll x){
+        i++;
+        while(i < (int)a.size()){
+            a[i] += x;
+            i += i & -i;
+        }
+    }
+    // sum of A[0, r)
+    ll sum(ll r) {
+        ll s = 0;
+        while(r){
+            s += a[r];
+            r -= r & -r;
+        }
+        return s;
+    }
+    // sum of A[l, r)
+    ll sum(ll l, ll r){  
+        return sum(r) - sum(l);
+    }
 };
--- src/data-structure/SegmentTree.hpp
+++ src/data-structure/SegmentTree.hpp
@@ -1,21 +1,38 @@
-struct Tree {
-   typedef int T;
-   static constexpr T unit = INT_MIN;
-   T f(T a, T b) { return max(a, b); }  // (any associative fn)
-   vector<T> s;
-   int n;
-   Tree(int n_ = 0, T def = unit){
-    ll log = 1;
-    while((1<<log) < n_) log++;
-    n = 1<<log;
-    s = vector<T>(n*2,def);
-   }
-   T query(int b, int e) {  // query [b, e)
-      T ra = unit, rb = unit;
-      for(b += n, e += n; b < e; b /= 2, e /= 2) {
-         if(b % 2) ra = f(ra, s[b++]);
-         if(e % 2) rb = f(s[--e], rb);
-      }
-      return f(ra, rb);
-   }
+template<class T>
+struct SegmentTree {
+    static constexpr T unit = INT_MAX;
+    T f(T a, T b) { return min(a, b); } 
+    vector<T> s;
+    int n;
+    SegmentTree(int n_ = 0, T def = unit) {
+        int log = 1; 
+        while ((1 << log) < n_) log++;
+        n = 1 << log;
+        s = vector<T>(n * 2, def);
+    }
+    // s[i] = x;
+    void update(int i, T x) {
+        i += n;
+        s[i] = x;
+        while (i >>= 1) {
+            s[i] = f(s[2 * i], s[2 * i + 1]);
+        }
+    }
+    // s[i] = f(s[i], x);
+    void apply(int i, T x) {
+        i += n;
+        s[i] = f(s[i], x);
+        while (i >>= 1) {
+            s[i] = f(s[2 * i], s[2 * i + 1]);
+        }
+    }
+    // 区間取得: [b, e)
+    T query(int b, int e) {
+        T ra = unit, rb = unit;
+        for (b += n, e += n; b < e; b /= 2, e /= 2) {
+            if (b % 2) ra = f(ra, s[b++]);
+            if (e % 2) rb = f(s[--e], rb);
+        }
+        return f(ra, rb);
+    }
 };
