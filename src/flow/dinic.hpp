edbdf67908d79d3ab03be5e4420150c0b9013857
mono-1729 <kawai.172957@gmail.com>
2025-12-02 19:29:33 +0900

    dinic変更

--- src/flow/dinic.hpp
+++ src/flow/dinic.hpp
@@ -1,53 +1,135 @@
-struct Dinic {
-   int V;                             // 頂点数
-   vector<vector<vector<ll>>> graph;  // グラフ
-   vector<int> dis;                   // 始点からの距離
-   vector<int> next;                  // 次に処理する頂点のメモ
-   Dinic(int v) : V(v) { graph.resize(V); }
-   void add_edge(int from, int to, ll capacity) {
-      graph[from].push_back({to, capacity, (int)graph[to].size()});
-      graph[to].push_back({from, 0, (int)graph[from].size() - 1});
-   }
-   void bfs(int s) {
-      dis.assign(V, -1);
-      dis[s] = 0;
-      deque<int> pos = {s};
-      while(pos.size()) {
-         int now = pos[0];
-         pos.pop_front();
-         for(auto& to : graph[now]) {
-            if(dis[to[0]] < 0 and to[1] > 0) {
-               dis[to[0]] = dis[now] + 1;
-               pos.emplace_back(to[0]);
+template <class Cap> 
+class Dinic {
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+    };
+    vector<pair<int, int>> pos;
+    vector<vector<_edge>> g;
+
+public:
+    Dinic(): _n(0) {}
+    explicit Dinic(int n): _n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap){
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = (int)pos.size();
+        pos.push_back({from, (int)g[from].size()});
+        int from_id = (int)g[from].size();
+        int to_id = (int)g[to].size();
+        if(from == to) to_id++;
+        g[from].push_back(_edge{to, to_id, cap});
+        g[to].push_back(_edge{from, from_id, 0});
+        return m;
+    }
+    // 最大流求めるだけならchange_edge()までなくても可
+    struct edge{
+        int from, to;
+        Cap cap, flow;
+    };
+    
+    edge get_edge(int i){
+        int m = (int)pos.size();
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return edge{pos[i].first, _e.to, _e.cap+_re.cap, _re.cap};
+    }
+
+    vector<edge> edges(){
+        int m = (int)pos.size();
+        vector<edge> result;
+        for(int i = 0; i < m; i++){
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+
+    void change_edge(int i, Cap new_cap, Cap new_flow){
+        int m = (int)pos.size();
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap-new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t){
+        return flow(s, t, numeric_limits<Cap>::max());
+    }
+    // s!=t である必要あり
+    Cap flow(int s, int t, Cap flow_limit){
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+        assert(s != t);
+
+        vector<int> level(_n), iter(_n);
+        queue<int> que;
+
+        auto bfs = [&]()->void {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            queue<int>().swap(que);
+            que.push(s);
+            while(!que.empty()){
+                int v = que.front(); que.pop();
+                for(auto e: g[v]){
+                    if(e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v]+1;
+                    if(e.to == t) return;
+                    que.push(e.to);
+                }
             }
-         }
-      }
-   }
-   ll dfs(int v, int t, ll f) {
-      if(v == t) return f;
-      for(int& i = next[v]; i < graph[v].size(); i++) {
-         int to = graph[v][i][0];
-         ll& cap = graph[v][i][1];
-         int rev = graph[v][i][2];
-         if(cap > 0 and dis[v] < dis[to]) {
-            ll d = dfs(to, t, min(f, cap));
-            if(d > 0) {
-               cap -= d;
-               graph[to][rev][1] += d;
-               return d;
+        };
+
+        auto dfs = [&](auto self, int v, Cap up)->Cap {
+            if(v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for(int& i = iter[v]; i < (int)g[v].size(); i++){
+                _edge& e = g[v][i];
+                if(level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d = self(self, e.to, min(up-res, g[e.to][e.rev].cap));
+                if(d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if(res == up) return res;
             }
-         }
-      }
-      return 0;
-   }
-   ll max_flow(int s, int t) {
-      ll flow = 0;
-      while(1) {
-         bfs(s);
-         if(dis[t] < 0) return flow;
-         next.assign(V, 0);
-         ll f;
-         while((f = dfs(s, t, INF)) > 0) flow += f;
-      }
-   }
+            level[v] = _n;
+            return res;
+        };
+        
+        Cap flow = 0;
+        while(flow < flow_limit){
+            bfs();
+            if(level[t] == -1) break;
+            fill(iter.begin(), iter.end(), 0);
+            Cap f = dfs(dfs, t, flow_limit-flow);
+            if(!f) break;
+            flow += f;
+        }
+        return flow;
+    }
+
+    // 最小カットをした上で、頂点 s 側に属する頂点集合を返す
+    vector<bool> min_cut(int s){
+        vector<bool> visited(_n);
+        queue<int> que;
+        while(!que.empty()){
+            int p = que.front(); que.pop();
+            visited[p] = true;
+            for(auto e: g[p]){
+                if(e.cap && !visited[e.to]){
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
 };
