7180a8a654ee4179b8a56935a3e9b07e621fb295
mono-1729 <kawai.172957@gmail.com>
2025-12-04 11:56:45 +0900

    lcaを修正

--- src/graph/tree/lca.hpp
+++ src/graph/tree/lca.hpp
@@ -1,67 +1,52 @@
-template<class graph> struct lca {
-   const graph& G;
-   vector<vector<int>> parent;
-   vector<int> dep;
-   int log = 1;
-   lca(const graph& G, int root = 0) : G(G) { init(root); }
-   void init(int root = 0) {
-      const int n = G.size();
-      dep.assign(n, -1);
-      parent.assign(1, vector<int>(n, -1));
-      queue<int> que({root});
-      dep[root] = 0;
-      int max_dep = 0;
-      while(!que.empty()) {
-         const int pos = que.front();
-         que.pop();
-         max_dep = max(max_dep, dep[pos]);
-         for(const auto& x : G[pos]) {
-            if(dep[x] == -1) {
-               dep[x] = dep[pos] + 1;
-               parent[0][x] = pos;
-               que.push(x);
+struct LCA {
+    int LOG;
+    vector<int> dep;
+    vector<vector<int>> par;
+
+    LCA(int n) : LOG(0), dep(n) {
+        while ((1 << LOG) <= n) LOG++;
+        par.assign(LOG, vector<int>(n, -1));
+    }
+
+    void build(const vector<vector<int>>& g, int root = 0) {
+        queue<int> q;
+        q.push(root);
+        dep[root] = 0;
+
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            for (int u : g[v]) {
+                if (u == par[0][v]) continue;
+                par[0][u] = v;
+                dep[u] = dep[v] + 1;
+                q.push(u);
             }
-         }
-      }
-      while((1 << log) <= max_dep) log++;
-      parent.resize(log, vector<int>(n, -1));
-      for(int k = 0; k < log - 1; k++) {
-         for(int v = 0; v < n; v++) {
-            if(parent[k][v] < 0) parent[k + 1][v] = -1;
-            else parent[k + 1][v] = parent[k][parent[k][v]];
-         }
-      }
-   }
-   int query(int u, int v) const {
-      if(dep[u] < dep[v]) swap(u, v);
-      const int sub = dep[u] - dep[v];
-      for(int k = 0; k < log; k++) {
-         if(!(sub >> k)) break;
-         if(sub >> k & 1) u = parent[k][u];
-      }
-      if(u == v) return u;
-      for(int k = __lg(dep[u]); k >= 0; k--) {
-         if(parent[k][u] != parent[k][v]) {
-            u = parent[k][u];
-            v = parent[k][v];
-         }
-      }
-      return parent[0][u];
-   }
-   int dist(const int u, const int v) const { return dep[u] + dep[v] - dep[query(u, v)] * 2; }
-   int jump(int u, int v, int d) const {
-      const int lc = query(u, v);
-      const int l = dep[u] - dep[lc];
-      const int r = dep[v] - dep[lc];
-      if(d < 0 || d > l + r) return -1;
-      if(l < d) {
-         d = l + r - d;
-         swap(u, v);
-      }
-      for(int k = 0; k < log; k++) {
-         if(!(d >> k)) break;
-         if(d >> k & 1) u = parent[k][u];
-      }
-      return u;
-   }
+        }
+
+        for (int k = 1; k < LOG; k++) {
+            for (int v = 0; v < (int)g.size(); v++) {
+                int p = par[k - 1][v];
+                par[k][v] = (p < 0 ? -1 : par[k - 1][p]);
+            }
+        }
+    }
+
+    int lca(int a, int b) const {
+        if (dep[a] < dep[b]) swap(a, b);
+        int d = dep[a] - dep[b];
+
+        for (int k = 0; k < LOG; k++) {
+            if (d >> k & 1) a = par[k][a];
+        }
+        if (a == b) return a;
+
+        for (int k = LOG - 1; k >= 0; k--) {
+            if (par[k][a] != par[k][b]) {
+                a = par[k][a];
+                b = par[k][b];
+            }
+        }
+        return par[0][a];
+    }
 };
